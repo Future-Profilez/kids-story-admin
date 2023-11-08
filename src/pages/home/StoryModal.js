0310b0d3be2c78d86d9826eba52b2e1352c5e6fe
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2023-11-08 11:51:20 +0530

    storymodal

--- src/pages/home/StoryModal.js
+++ src/pages/home/StoryModal.js
@@ -215,19 +215,24 @@ function StoryModal({ show, handleClose }) {
 
 
                                 ) : (
-                                    <div className="story-step-form" id="step4">
-                                        <div className="body-popup-title">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="115" height="116" viewBox="0 0 115 116" fill="none">
-                                                <path d="M105.438 13.9374C90.2517 14.0025 78.9628 16.0936 70.7938 19.7076C64.6979 22.4029 62.3125 24.4424 62.3125 31.3513V103.781C71.6495 95.3583 79.9354 92.9999 112.625 92.9999V13.9374H105.438ZM12 13.9374C27.1858 14.0025 38.4747 16.0936 46.6438 19.7076C52.7397 22.4029 55.125 24.4424 55.125 31.3513V103.781C45.788 95.3583 37.5021 92.9999 4.8125 92.9999V13.9374H12Z" fill="url(#paint0_linear_109_238)" />
-                                                <defs>
-                                                    <linearGradient id="paint0_linear_109_238" x1="1.14541" y1="30.2726" x2="117.174" y2="39.3206" gradientUnits="userSpaceOnUse">
-                                                        <stop stop-color="#4B69E2" />
-                                                        <stop offset="1" stop-color="#9054D9" />
-                                                    </linearGradient>
-                                                </defs>
-                                            </svg>
-                                            <h3>Please wait! while your story is being generated</h3>
-                                        </div>
+                                    <div className="succes" id="successpopup" >
+                                        <Modal.Body>
+                                            <div className="story-step-form">
+                                                <div className="body-popup-title">
+                                                    <svg width="115" height="116" viewBox="0 0 115 116" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M105.438 13.9374C90.2517 14.0025 78.9628 16.0936 70.7938 19.7076C64.6979 22.4029 62.3125 24.4424 62.3125 31.3513V103.781C71.6495 95.3583 79.9354 92.9999 112.625 92.9999V13.9374H105.438ZM12 13.9374C27.1858 14.0025 38.4747 16.0936 46.6438 19.7076C52.7397 22.4029 55.125 24.4424 55.125 31.3513V103.781C45.788 95.3583 37.5021 92.9999 4.8125 92.9999V13.9374H12Z" fill="url(#paint0_linear_109_238)" />
+                                                        <defs>
+                                                            <linearGradient id="paint0_linear_109_238" x1="1.14541" y1="30.2726" x2="117.174" y2="39.3206" gradientUnits="userSpaceOnUse">
+                                                                <stop stop-color="#4B69E2" />
+                                                                <stop offset="1" stop-color="#9054D9" />
+                                                            </linearGradient>
+                                                        </defs>
+                                                    </svg>
+                                                    <h3>Please wait! while your story is being generated </h3>
+                                                </div>
+                                            </div>
+
+                                        </Modal.Body>
                                     </div>
                                 )
                             )}

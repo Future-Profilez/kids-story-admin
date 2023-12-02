e28af184fb82a70e86199bb65654dc50ea460b2f
ankitjain0748 <ankitjain0748@gmail.com>
2023-12-02 11:14:01 +0530

    regenator  complte

--- src/pages/story/ImagePrompt.js
+++ src/pages/story/ImagePrompt.js
@@ -6,6 +6,7 @@ import Story from "../../Apis/Story";
 
 function ImagePrompt({ imageprompt }) {
     const [prompt, setPrompt] = useState(imageprompt);
+    console.log("Prompt ", prompt)
     const [modalShow, setModalShow] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [isClicked, setIsClicked] = useState(false);
@@ -20,7 +21,7 @@ function ImagePrompt({ imageprompt }) {
         formData.append('model_version', '1');
         formData.append('prompt', `${prompt} also modify image prompt`);
         formData.append('style_id', '30');
-        
+
         try {
             const response = await fetch(url, {
                 method: 'POST',
@@ -50,17 +51,23 @@ function ImagePrompt({ imageprompt }) {
     };
 
     const handleRegenerate = () => {
-        setIsLoading(true);
-        fetchData();
-    };
+         setIsLoading(true);
+        };
 
-    const handleContinue = () => {
-        setModalShow(false);
+ const handleContinue = () => {
+    setModalShow(false);
+     };
+    //
+
+    const handleGenerate = () => {
+        setModalShow(true);
+        setIsClicked(false);
+        setIsLoading(false);
     };
 
     return (
         <>
-            <div onClick={() => setModalShow(true)}>
+            <div onClick={handleGenerate}>
                 <img src={updatedImage} alt="story" />
             </div>
 
@@ -84,7 +91,8 @@ function ImagePrompt({ imageprompt }) {
                                     type="text"
                                     placeholder="Image Prompt"
                                     name="data"
-                                    defaultValue={prompt}
+                                    value={prompt}
+                                    onChange={(e) => setPrompt(e.target.value)}
                                     className="input_field form-control"
                                     id="password_field"
                                 />
@@ -99,25 +107,32 @@ function ImagePrompt({ imageprompt }) {
                         isClicked ? (
                             <div className="thumbnail-generating">
                                 <img src={updatedImage} alt="story" />
-                                </div>
-                        ) : (
-                            <div className="thumbnail-generating">
-                                <div className="image-loader">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10ZM3.5 10C3.5 13.5899 6.41015 16.5 10 16.5C13.5899 16.5 16.5 13.5899 16.5 10C16.5 6.41015 13.5899 3.5 10 3.5C6.41015 3.5 3.5 6.41015 3.5 10Z" fill="url(#paint0_angular_563_396)" />
-                                        <defs>
-                                            <radialGradient id="paint0_angular_563_396" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(10 10) rotate(90) scale(10)">
-                                                <stop stop-color="#9054D9" />
-                                                <stop offset="1" stop-color="#A04DFF" stop-opacity="0" />
-                                            </radialGradient>
-                                        </defs>
-                                    </svg>
-                                    Image Generating...
-                                </div>
-                                <img ref={imageRef} src={Story} alt="story" />
                             </div>
+                        ) : (
+                            <></>
                         )
                     )}
+                    {!isLoading ? (
+                        <></>
+                    ) : (
+                        <div className="thumbnail-generating">
+                            <div className="image-loader">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10ZM3.5 10C3.5 13.5899 6.41015 16.5 10 16.5C13.5899 16.5 16.5 13.5899 16.5 10C16.5 6.41015 13.5899 3.5 10 3.5C6.41015 3.5 3.5 6.41015 3.5 10Z" fill="url(#paint0_angular_563_396)" />
+                                    <defs>
+                                        <radialGradient id="paint0_angular_563_396" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(10 10) rotate(90) scale(10)">
+                                            <stop stop-color="#9054D9" />
+                                            <stop offset="1" stop-color="#A04DFF" stop-opacity="0" />
+                                        </radialGradient>
+                                    </defs>
+                                </svg>
+                                Image Generating...
+                            </div>
+                            <img ref={imageRef} src={Story} alt="story" />
+                        </div>
+                    )}
+
+
                     <div className="d-flex justify-content-around gap-3">
                         {isClicked && (
                             <div className="btn-list">
@@ -126,7 +141,7 @@ function ImagePrompt({ imageprompt }) {
                                 </button>
                                 <button className="btn blue-gradient-btn" onClick={handleContinue}>
                                     <span>Continue</span>
-                                    
+
                                 </button>
                             </div>
                         )}
